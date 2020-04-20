39f81985b14f5faa4892e25a1724e95f9e7e7d3d
Martin Terneborg <martinterneborg@protonmail.com>
2020-04-20 15:37:54 +0200

    (Lab3)Assignment 1 done

--- labs/lab3/app/Main.hs
+++ labs/lab3/app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import           Parser
+
 main :: IO ()
-main = print "Hello"
+-- main = print (spaces "     1abc")
+-- main = print (chars 5 "1234567")
+-- main = print ((require "abc") "abc123")
+-- main = print ((spaces #- digit) "   122")
+-- main = print ((spaces # digit) "   122")
+-- main = print (((spaces # digit) #> (\(a, b) -> Parser.return a)) "   122")
+-- main = print ((spaces # spaces) "   122")
+main = print (require "abc" "abc123")
--- labs/lab3/app/Parser.hs
+++ labs/lab3/app/Parser.hs
@@ -1,60 +1,79 @@
-module Parser(module CoreParser, T, digit, digitVal, chars, letter, err,
-              lit, number, iter, accept, require, token,
-              spaces, word, (-#), (#-)) where
-import Prelude hiding (return, fail)
-import Data.Char
-import CoreParser
-infixl 7 -#, #- 
+module Parser
+    ( module CoreParser
+    , T
+    , digit
+    , digitVal
+    , chars
+    , letter
+    , err
+    , lit
+    , number
+    , iter
+    , accept
+    , require
+    , token
+    , spaces
+    , word
+    , (-#)
+    , (#-)
+    )
+where
+import           Prelude                 hiding ( return
+                                                , fail
+                                                )
+import           Data.Char
+import           CoreParser
+infixl 7 -#, #-
 
 type T a = Parser a
 
 err :: String -> Parser a
-err message cs = error (message++" near "++cs++"\n")
+err message cs = error (message ++ " near " ++ cs ++ "\n")
 
-iter :: Parser a -> Parser [a]  
-iter m = m # iter m >-> cons ! return [] 
+iter :: Parser a -> Parser [a]
+iter m = m # iter m >-> cons ! return []
 
-cons(a, b) = a:b
+cons (a, b) = a : b
 
 (-#) :: Parser a -> Parser b -> Parser b
-m -# n = error "-# not implemented"
+m -# n = m # n >-> snd
 
 (#-) :: Parser a -> Parser b -> Parser a
-m #- n = error "#- not implemented"
+m #- n = m # n >-> fst
 
 spaces :: Parser String
-spaces =  error "spaces not implemented"
+spaces = iter (char ? isSpace)
 
 token :: Parser a -> Parser a
 token m = m #- spaces
 
 letter :: Parser Char
-letter =  error "letter not implemented"
+letter = char ? isAlpha
 
 word :: Parser String
 word = token (letter # iter letter >-> cons)
 
 chars :: Int -> Parser String
-chars n =  error "chars not implemented"
+chars n = iterate char n  where
+    iterate m 0 = return []
+    iterate m n = m # iterate m (n - 1) >-> cons
 
 accept :: String -> Parser String
-accept w = (token (chars (length w))) ? (==w)
+accept w = token (chars (length w)) ? (== w)
 
 require :: String -> Parser String
-require w  = error "require not implemented"
+require w = (token (chars (length w)) ? (== w)) ! err ("missing string " ++ w)
 
 lit :: Char -> Parser Char
-lit c = token char ? (==c)
+lit c = token char ? (== c)
 
-digit :: Parser Char 
-digit = char ? isDigit 
+digit :: Parser Char
+digit = char ? isDigit
 
 digitVal :: Parser Integer
 digitVal = digit >-> digitToInt >-> fromIntegral
 
 number' :: Integer -> Parser Integer
-number' n = digitVal #> (\ d -> number' (10*n+d))
-          ! return n
+number' n = digitVal #> (\d -> number' (10 * n + d)) ! return n
 number :: Parser Integer
 number = token (digitVal #> number')
-
