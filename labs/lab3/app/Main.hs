1464a1fac743cebfdf81d55c057b426aa3a20ee0
Martin Terneborg <martinterneborg@protonmail.com>
2020-04-22 09:11:51 +0200

    (Lab3)Assignment 4 done

--- labs/lab3/app/Main.hs
+++ labs/lab3/app/Main.hs
@@ -1,25 +1,23 @@
 module Main where
 
-import           Dictionary
-import           Statement
-
-d :: Dictionary.T String Integer
-d = Dictionary.insert ("a", 9) Dictionary.empty
+import           Program
 
 -- Should print 11, 13 and whatever you pass to it + 5
-whileLoop :: [Statement.T]
-whileLoop =
-    [ Statement.fromString "read n;"
-    , Statement.fromString "a:=2;"
-    , Statement.fromString "b:=5;"
-    , Statement.fromString "while 10 - a do begin a := a+1; b:=b+1; end"
-    , Statement.fromString "if a then skip; else a := a + 10;"
-    , Statement.fromString "a := a + 1;"
-    , Statement.fromString "n := n + 5;"
-    , Statement.fromString "write a;"
-    , Statement.fromString "write b;"
-    , Statement.fromString "write n;"
-    ]
+p :: Program.T
+p =
+    Program.fromString
+        "\ 
+    \ read n; \
+    \ a:=2; \
+    \ b:=5; \
+    \ while 10 - a do begin a := a+1; b:=b+1; end \
+    \ if a then skip; else a := a + 10; \
+    \ a := a + 1; \
+    \ n := n + 5; \
+    \ write a; \
+    \ write b; \
+    \ write n; \
+\ "
 
 main :: IO ()
-main = print $ exec whileLoop d [12]
+main = print $ Program.exec p [18]
--- labs/lab3/app/Program.hs
+++ labs/lab3/app/Program.hs
@@ -1,11 +1,25 @@
-module Program(T, parse, fromString, toString, exec) where
-import Parser hiding (T)
+module Program
+    ( T
+    , parse
+    , fromString
+    , toString
+    , exec
+    )
+where
+import           Parser                  hiding ( T )
 import qualified Statement
 import qualified Dictionary
-import Prelude hiding (return, fail)
-newtype T = Program () -- to be defined
+import           Prelude                 hiding ( return
+                                                , fail
+                                                )
+newtype T = Program [Statement.T] -- to be defined
+
+program :: Parser T
+program = iter (spaces -# Statement.parse #- spaces) >-> Program
+
 instance Parse T where
-  parse = error "Program.parse not implemented"
-  toString = error "Program.toString not implemented"
-             
-exec = error "Program.exec not implemented"
+    parse    = program
+    toString = error "Program.toString not implemented"
+
+exec :: Program.T -> [Integer] -> [Integer]
+exec (Program stmts) = Statement.exec stmts Dictionary.empty
